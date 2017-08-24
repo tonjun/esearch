fa537661c0419ee0dc7a2c456dedef91ccd77f84
Tonjun Banaag <tonjun@gmail.com>
2017-08-24 10:25:11 +0800

    Use RefreshIndex and add test case for Update()

--- esearch_test.go
+++ esearch_test.go
@@ -1,10 +1,10 @@
 package esearch
 
 import (
+	"encoding/json"
 	"log"
 	//"os"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -19,26 +19,28 @@ func TestESearch(t *testing.T) {
 
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile | log.Lmicroseconds)
 
+	idx := "tj-test-index"
+
 	es := NewESearch(&Options{URL: "http://localhost:9200"})
 	assert.NotNil(t, es)
 
-	es.DeleteIndex("tj-test-index")
+	es.DeleteIndex(idx)
 	//assert.Nil(t, err)
 
-	data := M{"owner": "u1", "message": "hello"}
+	data := M{"owner": "u1", "message": "hello", "seen": false}
 
-	err := es.Put("tj-test-index", "test", "doc1", data)
+	err := es.Put(idx, "test", "doc1", data)
 	assert.Nil(t, err)
 
 	// allow to save
-	time.Sleep(1 * time.Second)
+	es.RefreshIndex(idx)
 
 	query := M{
 		"query": M{
 			"term": M{"owner": "u1"},
 		},
 	}
-	res, err := es.Search("tj-test-index", "test", query)
+	res, err := es.Search(idx, "test", query)
 	assert.Nil(t, err)
 	assert.NotNil(t, res)
 	assert.Equal(t, int64(1), res.Hits.Total)
@@ -48,17 +50,83 @@ func TestESearch(t *testing.T) {
 		log.Printf("source: %s", string(*res.Hits.Hits[0].Source))
 	}
 
-	err = es.DeleteQuery("tj-test-index", "test", query)
+	err = es.DeleteQuery(idx, "test", query)
 	assert.Nil(t, err)
 
-	time.Sleep(1 * time.Second)
+	es.RefreshIndex(idx)
 
-	res, err = es.Search("tj-test-index", "test", query)
+	res, err = es.Search(idx, "test", query)
 	assert.Nil(t, err)
 	assert.NotNil(t, res)
 	assert.Equal(t, int64(0), res.Hits.Total)
 
-	err = es.DeleteIndex("tj-test-index")
+	err = es.DeleteIndex(idx)
+	assert.Nil(t, err)
+}
+
+func TestUpdate(t *testing.T) {
+
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile | log.Lmicroseconds)
+
+	idx := "myindex"
+
+	es := NewESearch(&Options{URL: "http://localhost:9200"})
+
+	es.DeleteIndex(idx)
+
+	data := M{"owner": "u2", "message": "hello there", "seen": false}
+
+	err := es.Put(idx, "test", "doc1", data)
+	assert.Nil(t, err)
+
+	es.RefreshIndex(idx)
+
+	query := M{
+		"query": M{
+			"term": M{"owner": "u2"},
+		},
+	}
+	res, err := es.Search(idx, "test", query)
+	assert.Nil(t, err)
+	assert.NotNil(t, res)
+	assert.Equal(t, int64(1), res.Hits.Total)
+
+	log.Printf("total: %d", res.Hits.Total)
+	if res.Hits.Total > 0 {
+		log.Printf("source: %s", string(*res.Hits.Hits[0].Source))
+		r := make(map[string]interface{})
+		err := json.Unmarshal(*res.Hits.Hits[0].Source, &r)
+		assert.Nil(t, err)
+		assert.Equal(t, r["owner"], "u2")
+		assert.Equal(t, r["message"], "hello there")
+		assert.Equal(t, r["seen"], false)
+	}
+
+	q := M{
+		"doc": M{
+			"seen": true,
+		},
+	}
+	es.Update(idx, "test", "doc1", q)
+
+	es.RefreshIndex(idx)
+
+	res, err = es.Search(idx, "test", query)
+	assert.Nil(t, err)
+	assert.NotNil(t, res)
+	assert.Equal(t, int64(1), res.Hits.Total)
+	log.Printf("total: %d", res.Hits.Total)
+	if res.Hits.Total > 0 {
+		log.Printf("source: %s", string(*res.Hits.Hits[0].Source))
+		r := make(map[string]interface{})
+		err := json.Unmarshal(*res.Hits.Hits[0].Source, &r)
+		assert.Nil(t, err)
+		assert.Equal(t, r["owner"], "u2")
+		assert.Equal(t, r["message"], "hello there")
+		assert.Equal(t, r["seen"], true)
+	}
+
+	err = es.DeleteIndex(idx)
 	assert.Nil(t, err)
 }
 
